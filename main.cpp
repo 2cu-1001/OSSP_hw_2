0da3098f9e73acb70d66a000dba371e1de4a322b
2cu1001 <leesiwoo021001@gmail.com>
2024-03-22 15:45:46 +0900

    2024-03-22-OSSP 개인과제#2

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string name, id;
+	cout << "이름을 입력하세요 : ";
+	cin >> name;
+	cout << "학번을 입력하세요 : ";
+	cin >> id;
+	cout << "<출력>\n" << name << "\n" << id;
+}

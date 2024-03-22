b8227896d810db1080e024ea294a73f5aeb5ca3f
2cu1001 <leesiwoo021001@gmail.com>
2024-03-22 15:52:10 +0900

    main.cpp 수정

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 int main()
 {
-	string name, id;
+	string name, id, teamName;
 	cout << "이름을 입력하세요 : ";
 	cin >> name;
 	cout << "학번을 입력하세요 : ";
 	cin >> id;
-	cout << "<출력>\n" << name << "\n" << id;
+	cout << "type team name : ";
+	cin >> teamName
+	cout << "<출력>\n" << name << "\n" << id << "\n" << teamName;
 }
